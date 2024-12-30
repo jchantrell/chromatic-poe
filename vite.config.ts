191fe7caa617477616cad3ae01516ccf1a7b8c84
Joel Chantrell <joelchantrell@protonmail.com>
2024-12-30 18:06:08 +1030

    ci: fix gh pages path issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,36 @@ import solid from "vite-plugin-solid";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  publicDir: "packages/assets",
-  plugins: [solid()],
-  resolve: {
-    alias: {
-      "@app": path.resolve(__dirname, "./apps/frontend"),
-      "@pkgs": path.resolve(__dirname, "./packages"),
+export default defineConfig(async ({ mode }) => {
+  const config = {
+    publicDir: "packages/assets",
+    plugins: [solid()],
+    resolve: {
+      alias: {
+        "@app": path.resolve(__dirname, "./apps/frontend"),
+        "@pkgs": path.resolve(__dirname, "./packages"),
+      },
     },
-  },
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      ignored: ["**/apps/backend/**"],
+    clearScreen: false,
+    server: {
+      port: 1420,
+      strictPort: true,
+      host: host || false,
+      hmr: host
+        ? {
+            protocol: "ws",
+            host,
+            port: 1421,
+          }
+        : undefined,
+      watch: {
+        ignored: ["**/apps/backend/**"],
+      },
     },
-  },
-}));
+  };
+
+  if (mode === "tauri") return config;
+  if (mode === "gh-pages") return { ...config, base: "/chromatic-poe/" };
+
+  return config;
+});
