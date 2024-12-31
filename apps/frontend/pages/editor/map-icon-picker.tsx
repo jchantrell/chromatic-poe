1b005175785a6c5c91718cb1bcf6e5c65ab318e0
Joel Chantrell <joelchantrell@protonmail.com>
2024-12-31 15:30:02 +1030

    fix(editor): another attempt at fixing img path issues in gh-pages

--- apps/frontend/pages/editor/map-icon-picker.tsx
+++ apps/frontend/pages/editor/map-icon-picker.tsx
@@ -13,7 +13,7 @@ import {
 import { Popover, PopoverContent, PopoverTrigger } from "@pkgs/ui/popover";
 import minimapIcons from "@pkgs/assets/poe2/minimap.json";
 import { RadioGroup, RadioGroupItem } from "@pkgs/ui/radio-group";
-import { BASE_URL } from "@app/app";
+import minimapImg from "@pkgs/assets/poe2/images/Art@2DArt@Minimap@Player.png";
 
 const SHEET_WIDTH = 896;
 const SHEET_HEIGHT = 3776;
@@ -28,7 +28,7 @@ export function MinimapIcon(props: {
   return (
     <div
       style={{
-        "background-image": `url('${BASE_URL}/poe2/images/art@2dart@minimap@player.png')`,
+        "background-image": `url(${minimapImg})`,
         height: `calc(64px / ${props.scale})`,
         width: `calc(64px / ${props.scale})`,
         "background-position": `calc(-${minimapIcons[props.color][props.shape][props.size].x}px / ${props.scale}) calc(-${minimapIcons[props.color][props.shape][props.size].y}px / ${props.scale})`,
