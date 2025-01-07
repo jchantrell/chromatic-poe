a5f338f7f4ab43d29816d38f374efde4bb6d73ef
Joel Chantrell <joelchantrell@protonmail.com>
2025-01-08 09:46:21 +1030

    ci: always fetch latest patch version

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+POE1_PATCH_URL="https://lvlvllvlvllvlvl.github.io/poecdn-bundle-index/poe1/urls.json"
+POE2_PATCH_URL="https://lvlvllvlvllvlvl.github.io/poecdn-bundle-index/poe2/urls.json"
+POE1_VERSION=$(curl $POE1_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
+POE2_VERSION=$(curl $POE2_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
+
+
 rm -f chromatic.db
 
 echo "Installing dependencies"
@@ -9,6 +15,8 @@ pnpm install --frozen-lockfile
 echo "Fetching game files from patch servers"
 mkdir -p packages/assets/poe2/images
 pushd packages/data/poe2
+echo "Using PoE2 version: $POE2_VERSION"
+cat config.json | jq '.patch = "'$POE2_VERSION'"' > temp.json && mv temp.json config.json
 npx pathofexile-dat
 popd
 
