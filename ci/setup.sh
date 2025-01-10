6fd4e8ef27d74ed995914fc3599fd086b4e9c23c
Joel Chantrell <joelchantrell@protonmail.com>
2025-01-10 11:00:57 +1030

    ci: manually set patch version for now

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,8 +3,8 @@ set -e
 
 POE1_PATCH_URL="https://lvlvllvlvllvlvl.github.io/poecdn-bundle-index/poe1/urls.json"
 POE2_PATCH_URL="https://lvlvllvlvllvlvl.github.io/poecdn-bundle-index/poe2/urls.json"
-POE1_VERSION=$(curl $POE1_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
-POE2_VERSION=$(curl $POE2_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
+POE1_VERSION=$(curl -s $POE1_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
+POE2_VERSION=$(curl -s $POE2_PATCH_URL | jq -r '.urls[0] | split("/")[-2]')
 
 
 rm -f chromatic.db
@@ -15,8 +15,8 @@ pnpm install --frozen-lockfile
 echo "Fetching game files from patch servers"
 mkdir -p packages/assets/poe2/images
 pushd packages/data/poe2
-echo "Using PoE2 version: $POE2_VERSION"
-cat config.json | jq '.patch = "'$POE2_VERSION'"' > temp.json && mv temp.json config.json
+# echo "Using PoE2 version: $POE2_VERSION"
+# cat config.json | jq '.patch = "'$POE2_VERSION'"' > temp.json && mv temp.json config.json
 npx pathofexile-dat
 popd
 
